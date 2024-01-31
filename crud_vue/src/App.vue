166f9e7cabdb68ff8abceb5fd6c198a58f4ec110
jbcjpa <joshuacelis020602.com>
2024-01-31 16:31:57 +0800

    innitial commit

---
+++ crud_vue/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import  { ref } from 'vue';
+let counter = ref(0);
+
+function add(){
+  counter.value++;
+  
+}
+
+function sub(){
+  counter.value--;
+  
+}
+  
+</script>
+
+<template>
+  <div>
+    <h1>{{ counter }}</h1>
+    <button @click="add()"> + </button>
+    <button @click="sub()"> - </button>
+  </div>
+</template>
+
+<style>
+ div {
+  text-align: center;
+ }
+</style>
---
+++ index.js
@@ -0,0 +1,29 @@
+let counter = document.getElementById("counter").innerHTML
+
+function add() {
+    let new_counter = parseInt(counter);
+    new_counter++;
+    counter = new_counter;
+    console.log(counter);
+}
+
+function add() {
+    let new_counter = parseInt(counter);
+    new_counter++;
+    counter = new_counter;
+    console.log(counter);
+}
+
+function add() {
+    let new_counter = parseInt(counter);
+    new_counter++;
+    counter = new_counter;
+    console.log(counter);
+}
+
+function add() {
+    let new_counter = parseInt(counter);
+    new_counter++;
+    counter = new_counter;
+    console.log(counter);
+}
